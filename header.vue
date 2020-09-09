215d1bd1470eab204153b8679a5f0678ef297b7e
你的姓名 <a14235>
2020-09-09 23:31:48 +0800

    团队项目第一次提交

---
+++ header.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+      <!-- 顶部搜索框开始 -->
+        <div>
+            <mt-search dixed v-model="value"  ></mt-search>
+         </div>
+       <!--顶部搜索框结束  -->
+       <!--顶部选项开始  -->
+       <div class="fixed">
+        <mt-navbar >
+                <mt-tab-item >美食</mt-tab-item>
+                <mt-tab-item >超市</mt-tab-item>
+                <mt-tab-item >饮品</mt-tab-item>
+                <mt-tab-item >精选</mt-tab-item>
+            </mt-navbar>
+        </div>
+       <!--顶部选项结束  -->
+     </div>  
+</template>
+<style scoped>
+.container{
+  margin-top:40px;
+}
+</style>
